9bd634cc6505ea2c6f9d48373ddb4f490356c4c9
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-05-24 15:12:59 +0300

    Completed coding of the singleton creational design pattern

---
+++ ClassLibrary/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary.CreationalPatterns.Singleton;
+
+public class Singleton
+{
+    private static Singleton? _instance;
+    private static object _lock = new object();
+
+    private Singleton() { }
+
+    public static Singleton GetInstance() { if (_instance == null) { lock (_lock) { _instance = new Singleton(); } } return _instance; }
+    public string HelloWorld() => "Hello world!";
+}
--- ConsoleApplication/Main.cs
+++ ConsoleApplication/Main.cs
@@ -1,6 +1,19 @@
-﻿namespace ConsoleApplication;
+﻿namespace ClassLibrary.CreationalPatterns.Singleton;
 
-internal class Program
+public class Client
 {
-    static void Main(string[] args) { }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("App: Launched with");
+        Singleton singleton = Singleton.GetInstance();
+        Console.WriteLine(singleton.HelloWorld());
+
+        Console.WriteLine();
+
+        Singleton singleton2 = Singleton.GetInstance();
+        singleton2.HelloWorld();
+
+        if (singleton == singleton2) { Console.WriteLine("App: Singleton works, both variables contain the same instance."); }
+        else { Console.WriteLine("App: Singleton failed, variables contain different instances."); }
+    }
 }
