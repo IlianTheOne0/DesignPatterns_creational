5cda33ac7742401d1fafc021f8317b531c4400ad
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-05-24 14:10:38 +0300

    Completed coding of the prototype creational design pattern

---
+++ ClassLibrary/Prototype.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassLibrary.CreationalPatterns.Prototype.NPrototype;
+
+public interface IPrototype
+{
+    IPrototype Clone();
+}
+
+public abstract class Prototype : IPrototype
+{
+    public abstract IPrototype Clone();
+}
+
+public class Object : Prototype
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public int z { get; set; }
+
+    public int width { get; set; }
+    public int height { get; set; }
+    public int depth { get; set; }
+
+    public Object(int x, int y, int z, int width, int height, int depth)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public override Prototype Clone() => new Object(x, y, z, width, height, depth);
+}
--- ConsoleApplication/Main.cs
+++ ConsoleApplication/Main.cs
@@ -1,6 +1,24 @@
-﻿namespace ConsoleApplication;
+﻿namespace ClassLibrary.CreationalPatterns.Prototype;
+using ClassLibrary.CreationalPatterns.Prototype.NPrototype;
 
-internal class Program
+public class Client
 {
-    static void Main(string[] args) { }
+    public static void ClientMethod(Object _object)
+    {
+        Console.WriteLine($"Coords: ({_object.x}; {_object.y}; {_object.z})");
+        Console.WriteLine($"Sizes: ({_object.width}; {_object.height}; {_object.depth})");
+    }
+
+    public static void Main(string[] args)
+    {
+        Object _object1 = new Object(-1, -1, -1, 2, 2, 2);
+        Console.WriteLine("App: Launched with the Object.");
+        ClientMethod(_object1);
+
+        Console.WriteLine();
+
+        Object _object2 = (Object)_object1.Clone();
+        Console.WriteLine("App: Launched with the Object2.");
+        ClientMethod(_object2);
+    }
 }
